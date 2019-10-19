8a144d213cac5ec074f7b1a3774c8144cdda3b77
soroush behmardi <sori.bk@gmail.com>
2019-10-18 18:11:11 -0700

    add yml file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <div className="topleft">
           <p>POLGOZAR</p>
         </div>
-        <div className="middle">
+        <div className="middle">   
           <h1>COMING SOON</h1>
           <hr/>
         </div>
