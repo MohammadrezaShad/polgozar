fc8f5e8d19e03661d0c91b8fe27d2515f11d73c2
soroush behmardi <sori.bk@gmail.com>
2020-09-02 17:39:37 +0300

    add days

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -7,39 +7,45 @@ import { Icon } from "antd";
 import _ from "lodash";
 
 const ZDays = [
-  "anaram",
-  "urmazd",
-  "vahman",
-  "ardibehesht",
-  "shahrivar",
-  "spandarmaz",
-  "khordad",
-  "amordad",
-  "dey be azar",
-  "azar",
-  "aban",
-  "khor(khir)",
-  "mah",
-  "tir",
-  "goosh",
-  "dey be mehr",
-  "mehr",
-  "soroush",
-  "rashn",
-  "farvardin",
-  "verahram",
-  "ram",
-  "bad",
-  "dey be din",
-  "din",
-  "ard ",
-  "ashtad",
-  "asman",
-  "zamiyad",
-  "mantraspand"
+  { en: "urmazd", fr: "اورمزد" },
+  { en: "vahman", fr: "بهمن" },
+  { en: "ardibehesht", fr: "اردیبهشت" },
+  { en: "shahrivar", fr: "شهریور" },
+  { en: "spandarmaz", fr: "سپندارمذ" },
+  { en: "khordad", fr: "خرداد" },
+  { en: "amordad", fr: "امرداد" },
+  { en: "dey be azar", fr: "دی به آذر" },
+  { en: "azar", fr: "آذر" },
+  { en: "aban", fr: "آبان" },
+  { en: "khor(khir)", fr: "خور" },
+  { en: "mah", fr: "ماه" },
+  { en: "tir", fr: "تیر" },
+  { en: "goosh", fr: "گوش" },
+  { en: "dey be mehr", fr: "دی به مهر" },
+  { en: "mehr", fr: "مهر" },
+  { en: "soroush", fr: "سروش" },
+  { en: "rashn", fr: "رشن" },
+  { en: "farvardin", fr: "فروردین" },
+  { en: "verahram", fr: "ورهرام" },
+  { en: "ram", fr: "رام" },
+  { en: "bad", fr: "یاد" },
+  { en: "dey be din", fr: "دی بدین" },
+  { en: "din", fr: "دین" },
+  { en: "ard ", fr: "ارد" },
+  { en: "ashtad", fr: "اشتاد" },
+  { en: "asman", fr: "آسمان" },
+  { en: "zamiyad", fr: "زامیاد" },
+  { en: "mantraspand", fr: "مانترسپند" },
+  { en: "anaram", fr: "انارام" }
 ];
-const extraZdays = ["vahooshtoesh", "ahnavad", "oshtavad", "sepantamad", "vahookhashatr"];
-const NaborsDay = ["vahman", "mah", "goosh", "ram"];
+const extraZdays = [
+  { en: "ahnavad", fr: "اهنود" },
+  { en: "oshtavad", fr: "اشتود" },
+  { en: "sepantamad", fr: "سپنتمد" },
+  { en: "vahookhashatr", fr: "وهوخشتر" },
+  { en: "vahooshtoesh", fr: "وهشتواش" }
+];
+const NaborsDay = [1, 11, 13, 20];
 class Home extends Component {
   state = {
     dateIdx: 0
