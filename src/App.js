3470b6c1d1f4ff7717241deebf3d40d0902096ab
soroush <sori.bk@gmail.com>
2020-11-16 22:58:05 -0800

    remove unused stuff

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import AppStyle from './App.style';
 import { ThemeProvider } from 'styled-components';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './containers/Home';
 import theme from './settings/theme';
 
 const App = () => (
@@ -13,7 +12,7 @@ const App = () => (
         <Router>
           <Switch>
             <Route exact path="/">
-              <Home />
+              <div>fsad</div>
             </Route>
           </Switch>
         </Router>
