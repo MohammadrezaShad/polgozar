61f22547dc6460d6d06124e514a3b9f54062fe9e
soroush <sori.bk@gmail.com>
2020-11-28 17:59:07 -0800

    remove style ts file

--- src/settings/style.ts
+++ src/settings/style.ts
@@ -1,4 +1,4 @@
-import { css } from 'styled-components';
+import { css, ThemedCssFunction, DefaultTheme } from 'styled-components';
 import { sizes, colors, spacer } from './styleConfig';
 
 export {
@@ -15,26 +15,25 @@ export {
   fontType,
 } from './styleConfig';
 
-export const maxWinSize = (size) => {
+export type SizeType = keyof typeof sizes;
+export type ColorTypes = keyof typeof colors;
+
+export const maxWinSize = (size: SizeType) => {
   const windowSize = window.innerWidth;
   return windowSize <= parseInt(sizes[size].replace('px', ''), 10);
 };
 
 export const media = Object.keys(sizes).reduce(
-  (acc, size) => {
-    acc[size] = (...args) => css`
-      @media only screen and (max-width: ${sizes[size]}) {
-        ${css(...args)}
+  (acc, size) => ({
+    ...acc,
+    [size]: (prop: any) => css`
+      @media only screen and (max-width: ${sizes[size as SizeType]}) {
+        ${css(prop)}
       }
-    `;
-    return acc;
-  },
-  { ...sizes },
-);
-
-export const rem = (pixels) => {
-  return `${pixels / 16}rem;`;
-};
+    `,
+  }),
+  {},
+) as { [size in SizeType]: ThemedCssFunction<DefaultTheme> };
 
 export const snippet = {
   underline: (color = colors.font) => css`
