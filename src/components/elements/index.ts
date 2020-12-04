e5c5ddc6ff4420c02f867c2dc0b4127f6c3fe396
unknown <shadmobaraki@plaza.ir>
2020-12-04 20:56:09 +0330

    add tabs and stars export

--- src/components/elements/index.ts
+++ src/components/elements/index.ts
@@ -4,3 +4,5 @@ export { default as HeroImage } from './heroImg';
 export { default as Modal } from './modal';
 export { default as FormItem } from './formItem';
 export { default as Spinner } from './spinner';
+export { default as Tabs } from './tabs/tabs';
+export { default as Stars } from './star-rating/star-rating';
