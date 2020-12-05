54d0c9d6596a22a87a88e36e4c44dbea72a84ca8
unknown <shadmobaraki@plaza.ir>
2020-12-05 23:06:17 +0330

    add avatar

---
+++ src/components/elements/avatar/avatar.styled.ts
@@ -0,0 +1,35 @@
+import { colors } from 'settings/style';
+import styled from 'styled-components';
+
+interface AvatarProps {
+  size?: 'md' | 'lg';
+}
+export const StyledImgWrap = styled.div<AvatarProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  height: ${({ size }) => (size === 'md' ? '70px' : '200px')};
+  width: ${({ size }) => (size === 'md' ? '70px' : '200px')};
+  background-color: ${colors.white};
+`;
+
+export const StyledImgContainer = styled.div<AvatarProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  height: ${({ size }) => (size === 'md' ? '62px' : '182px')};
+  width: ${({ size }) => (size === 'md' ? '62px' : '182px')};
+  background: ${colors.primary300};
+  background: ${`linear-gradient(137deg, ${colors.primary300} 29%, ${colors.accent} 100%)`};
+`;
+
+export const StyledImg = styled.img<AvatarProps>`
+  display: block;
+  border-radius: 50%;
+  height: ${({ size }) => (size === 'md' ? '60px' : '172px')};
+  width: ${({ size }) => (size === 'md' ? '60px' : '172px')};
+  background-color: ${colors.white};
+  object-fit: cover;
+`;
