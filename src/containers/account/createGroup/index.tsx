d734f0c849fb19de9f3f183ecf1d4e858ab59a20
soroush <sori.bk@gmail.com>
2020-12-09 22:32:50 -0800

    add stepbarForm

--- src/containers/account/createGroup/index.tsx
+++ src/containers/account/createGroup/index.tsx
@@ -1,11 +1,9 @@
-import React, { useState, useCallback } from 'react';
-import styled from 'styled-components';
-import { StepBar, Button } from 'components/elements';
-import { colors, spacer, fontType, radius, sizes } from 'settings/style';
+import React from 'react';
 import { Form, message } from 'antd';
 import { useCreateGroupMutation } from 'graphql/types';
 import { useHistory } from 'react-router-dom';
 import { ResizeImageResult, ParseGoogleAddressResult } from 'helpers';
+import StepBarForm from 'components/stepBarForm';
 import CoverPhotoStep from './coverPhotoStep';
 import InformationStep from './informationStep';
 import AddCategoriesStep from './addCategoriesStep';
@@ -36,24 +34,9 @@ const steps = [
 ];
 
 function CreateGroup() {
-  const [step, setStep] = useState(1);
   const [createGroupForm] = Form.useForm();
   const [createGroup, { loading: createGroupLoading }] = useCreateGroupMutation();
   const history = useHistory();
-
-  const onNext = useCallback(async () => {
-    try {
-      const validation = steps[step - 1]?.validation;
-      await createGroupForm.validateFields(validation);
-      if (step === steps.length) {
-        createGroupForm.submit();
-      } else {
-        setStep(step + 1);
-      }
-    } catch (err) {
-      // err
-    }
-  }, [createGroupForm, step]);
 
   const onFinish = async (values: FromFields) => {
     try {
@@ -70,74 +53,14 @@ function CreateGroup() {
   };
 
   return (
-    <Wrapper>
-      <ProgressBarHeader>
-        <HeaderTitle>Make new Group</HeaderTitle>
-        <StepBar step={step} steps={steps} />
-      </ProgressBarHeader>
-      <ContentWrapper>
-        <FormWrapper>
-          <Form layout="vertical" form={createGroupForm} onFinish={onFinish}>
-            <FormBoxWrapper>
-              {steps.map((item, index) => (
-                <div style={{ display: index + 1 === step ? 'block' : 'none' }} key={item.title}>
-                  <StepTitle>{item.pageTitle}</StepTitle>
-                  {item.content()}
-                </div>
-              ))}
-            </FormBoxWrapper>
-          </Form>
-          <FormFooter>
-            {step > 1 ? <Button onClick={() => setStep(step - 1)}>Back</Button> : <span />}
-            <Button onClick={onNext} isLoading={createGroupLoading}>
-              {step === steps.length ? 'Confirm' : 'Continue'}
-            </Button>
-          </FormFooter>
-        </FormWrapper>
-      </ContentWrapper>
-    </Wrapper>
+    <StepBarForm
+      pageTitle="Make new Group"
+      form={createGroupForm}
+      onFinish={onFinish}
+      isLoading={createGroupLoading}
+      steps={steps}
+    />
   );
 }
 
 export default CreateGroup;
-
-const Wrapper = styled.div`
-  width: 100%;
-`;
-
-const HeaderTitle = styled.h2`
-  ${fontType.lg}
-  padding-bottom: ${spacer.lg};
-`;
-
-const ContentWrapper = styled.div`
-  background-color: #eeeeee;
-  padding: ${spacer.xl};
-`;
-
-const ProgressBarHeader = styled.div`
-  padding: ${spacer.xl};
-`;
-
-const FormWrapper = styled.div`
-  max-width: ${sizes.md};
-  margin: auto;
-`;
-
-const FormBoxWrapper = styled.div`
-  background-color: ${colors.white};
-  border-radius: ${radius.md};
-  padding: ${spacer.xl};
-  margin-bottom: ${spacer.lg};
-`;
-
-const FormFooter = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
-const StepTitle = styled.p`
-  ${fontType.md};
-  margin-bottom: ${spacer.xl};
-`;
